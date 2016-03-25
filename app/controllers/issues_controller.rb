746ccb1dce2c00a98ac51f9fc522151554956576
Izuroxx <izu.i.cnr@gmail.com>
2016-03-25 22:39:53 +0100

    add views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,22 @@
 class IssuesController < ApplicationController
 
 	def index
+		@issues = Issue.all
+
 	end
+
+	def new
+	end
+
+	def create
+		@issue = Issue.new(issue_params)
+		@issue.save
+
+		redirect_to @issue
+	end
+
+	def show
+	end
+
 
 end
